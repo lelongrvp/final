ee20850d07c2d10a43396731c66ac10f290a87fb
Le Duc Long <lelongrvp7@gmail.com>
2023-07-11 08:49:43 +0700

    adding change

--- src/Movie/Movie.jsx
+++ src/Movie/Movie.jsx
@@ -1,12 +1,8 @@
-import { Typography } from "antd";
 import MovieCard from "./MovieCard";
-
-const { Title } = Typography;
 
 const Movie = () => {
   return (
     <>
-      <Title>This is a movie app</Title>
       <MovieCard />
     </>
   );
